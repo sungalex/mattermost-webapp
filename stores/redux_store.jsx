11628b98825cff712965934a2cd404442910105b
Harrison Healey <harrisonmhealey@gmail.com>
2022-06-17 17:48:20 -0400

    Always expose make store available for debugging on community and remove unused function

--- stores/redux_store.jsx
+++ stores/redux_store.jsx
@@ -1,20 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-// This is a temporary store while we are transitioning from Flux to Redux. This file exports
-// the configured Redux store for use by actions and selectors.
-
 import configureStore from 'store';
 
 const store = configureStore();
 
-export function bindActionToRedux(action, ...args) {
-    return async () => {
-        await action(...args)(store.dispatch, store.getState);
-    };
-}
-
-if (process.env.NODE_ENV !== 'production') { //eslint-disable-line no-process-env
+// eslint-disable-next-line no-process-env
+if (process.env.NODE_ENV !== 'production' || window.location.origin === 'https://community.mattermost.com') {
     window.store = store;
 }
 
